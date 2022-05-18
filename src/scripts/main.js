6f9240bdaf8397e5234639c2fc2a99f0c9af72e6
Sariah Campopiano <scampopiano925@gmail.com>
2022-05-18 14:13:06 -0500

    Created API call. Used promise and targeted the HTML to render song lyrics to the DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,26 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
+// API call
+const getLyrics = () => new Promise((resolve, reject) => {
+  axios.get('https://api.lyrics.ovh/v1/twenty one pilots/redecorate')
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+// UI Presentation (HTML on the DOM)
+const lyricsOnDom = () => {
+  // Below is a promise, so I have to use .then with the promise
+  getLyrics().then((response) => {
+    document.querySelector('#app').innerHTML = response.lyrics;
+  });
 };
 
-init();
+const startApp = () => {
+  lyricsOnDom();
+};
+
+startApp();
